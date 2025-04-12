763b7f24827e0113a26efaf157eaa9e5e194cab0
daniel <djuanrique@gmail.com>
2025-04-12 11:28:57 -0500

    commit

--- lib/widgets/custom_navigationbar.dart
+++ lib/widgets/custom_navigationbar.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_zarzal/providers/ui_providers.dart';
 
 class CustomNavigationbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+    final uiProviders = Provider.of<UiProviders>(context);
+    final currentIndex = uiProviders.selectedMenuOpt;
     return BottomNavigationBar(
+        onTap: (int i) => uiProviders.selectedMenuOpt = i,
         currentIndex: currentIndex,
         elevation: 0,
         items: <BottomNavigationBarItem>[
